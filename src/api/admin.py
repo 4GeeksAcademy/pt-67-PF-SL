4c956e2e4d18439e9aff050d38f61e6ee9daa71d
Sonia <161872642+Inflanubes@users.noreply.github.com>
2024-08-12 18:04:03 +0000

    Endpoints,models,routes

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,8 +1,13 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Order, OrderItems, Photo
 from flask_admin.contrib.sqla import ModelView
+
+class OrderItemsView(ModelView):
+    column_list = ('order_id', 'photo_id')
+    form_columns = ('order_id', 'photo_id')
+    can_create = True
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
@@ -12,6 +17,8 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-
+    admin.add_view(ModelView(Order, db.session))
+    admin.add_view(ModelView(Photo, db.session))
+    admin.add_view(OrderItemsView(OrderItems, db.session))
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
