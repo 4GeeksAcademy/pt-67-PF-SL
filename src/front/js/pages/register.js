0b5c00629ec7cad3c79bd5ed1bec3e9473459166
Sonia <161872642+Inflanubes@users.noreply.github.com>
2024-08-14 19:29:34 +0000

    Resumir Imports login y register

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Context } from '../store/appContext';
-import { Link } from 'react-router-dom';
 
 const register = () => {
     const {actions} = useContext(Context)
